f3483803419e3efa5a73ce7c8f8cf79637862c46
omouravictor <omouravictor@gmail.com>
2021-12-16 20:35:28 -0300

    Task and date input Edit Text changed to TextInputEditText and clearBtn added

--- app/src/main/java/com/example/tasks/AddActivity.java
+++ app/src/main/java/com/example/tasks/AddActivity.java
@@ -3,40 +3,48 @@ package com.example.tasks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.DatePicker;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
 
-    EditText etDate;
+    EditText etTask, etDate;
+    Button clearBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        etDate = findViewById(R.id.etDate);
-
         Calendar calendar = Calendar.getInstance();
-        final int year = calendar.get(Calendar.YEAR);
-        final int month = calendar.get(Calendar.MONTH);
-        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        final int currentYear = calendar.get(Calendar.YEAR);
+        final int currentMonth = calendar.get(Calendar.MONTH);
+        final int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        etTask = findViewById(R.id.inputEditTextTask);
+        etDate = findViewById(R.id.inputEditTextDate);
+        clearBtn = findViewById(R.id.clearBtn);
 
         etDate.setOnClickListener((v) -> {
             DatePickerDialog datePickerDialog = new DatePickerDialog(
-                    AddActivity.this, (view, year1, month1, day1) -> {
-                month1 = month1 + 1;
-                String date = day1 + "/" + month1 + "/" + year1;
+                    AddActivity.this, (view, year, month, day) -> {
+                month = month + 1;
+                String date = day + "/" + month + "/" + year;
                 etDate.setText(date);
-            }, year, month, day);
+            }, currentYear, currentMonth, currentDay);
             datePickerDialog.show();
+        });
+
+        clearBtn.setOnClickListener((v) -> {
+            Toast.makeText(getApplicationContext(), "Exemplo Toast", Toast.LENGTH_SHORT).show();
+//            etTask.setText("");
+//            etDate.setText("");
         });
     }
 }
