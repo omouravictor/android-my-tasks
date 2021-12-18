b6b70bcebf90688f485bc44424e6532861abcca2
omouravictor <omouravictor@gmail.com>
2021-12-18 14:15:05 -0300

    TaskModel created

---
+++ app/src/main/java/com/example/tasks/TaskModel.java
@@ -0,0 +1,49 @@
+package com.example.tasks;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class TaskModel {
+
+    private int id;
+    private String name;
+    private String slaDate;
+
+    public TaskModel() {
+    }
+
+    public TaskModel(int id, String name, String slaDate) {
+        this.id = id;
+        this.name = name;
+        this.slaDate = slaDate;
+    }
+
+    public TaskModel(String name, String slaDate) {
+        this.name = name;
+        this.slaDate = slaDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlaDate() {
+        return slaDate;
+    }
+
+    public void setSlaDate(String dateSLA) {
+        this.slaDate = dateSLA;
+    }
+}
