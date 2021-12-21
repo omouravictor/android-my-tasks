674162f442044a9a07e153e1cac07b6c89074adc
omouravictor <omouravictor@gmail.com>
2021-12-20 22:50:17 -0300

    MainActivity updated

--- app/src/main/java/com/example/tasks/MainActivity.java
+++ app/src/main/java/com/example/tasks/MainActivity.java
@@ -1,29 +1,43 @@
 package com.example.tasks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton btnAdd;
     RecyclerView recyclerView;
-    FloatingActionButton add_button;
+    TaskAdapter adapter;
+    SQLiteHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startVariables();
+        startRecyclerView();
 
-        recyclerView = findViewById(R.id.recyclerView);
-        add_button = findViewById(R.id.add_button);
-        add_button.setOnClickListener(view -> {
+        btnAdd.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, AddActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void startVariables() {
+        myDB = new SQLiteHelper(this);
+        adapter = new TaskAdapter(myDB.getAllTasks());
+        btnAdd = findViewById(R.id.btnAdd);
+    }
+
+    private void startRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
