2a2fd84ffbd344e3bbe5d99c3ee83298d0f4ff30
omouravictor <omouravictor@gmail.com>
2021-12-21 22:45:25 -0300

    'MainActivity.this' changed to 'this' on intent

--- app/src/main/java/com/example/tasks/MainActivity.java
+++ app/src/main/java/com/example/tasks/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         startRecyclerView();
 
         btnAdd.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, AddActivity.class);
+            Intent intent = new Intent(this, AddActivity.class);
             startActivity(intent);
         });
     }
