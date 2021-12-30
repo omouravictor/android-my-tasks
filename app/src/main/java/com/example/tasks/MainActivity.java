a6518c8f52398c28378df071a87ab1bee7debd0f
omouravictor <omouravictor@gmail.com>
2021-12-29 22:36:59 -0300

    setAdapterOnClickListenerS() created

--- app/src/main/java/com/example/tasks/MainActivity.java
+++ app/src/main/java/com/example/tasks/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.tasks;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -24,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        setAdapterOnClickListener();
         setBtnAddOnClickListener();
+        setAdapterOnClickListenerS();
     }
 
     private void initView() {
@@ -55,13 +59,43 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
-    public void setAdapterOnClickListener() {
-        adapter.setOnClickListenerInterface(position -> v -> {
-            Intent intent = new Intent(this, UpdateActivity.class);
-            TaskModel task = adapter.getTask(position);
-            intent.putExtra("task", task);
-            intent.putExtra("position", position);
-            mainActivityResult.launch(intent);
+    public void setAdapterOnClickListenerS() {
+        adapter.setOnClickListenerInterface(new AdapterInterface() {
+            @Override
+            public View.OnClickListener getOnClickListener(int position) {
+                return v -> {
+                    Intent intent = new Intent(v.getContext(), UpdateActivity.class);
+                    TaskModel task = adapter.getTask(position);
+                    intent.putExtra("task", task);
+                    intent.putExtra("position", position);
+                    mainActivityResult.launch(intent);
+                };
+            }
+
+            @Override
+            public View.OnClickListener getBtnCompleteOnClickListener(int position) {
+                return v -> {
+                    Context context = v.getContext();
+                    TaskModel task = adapter.getTask(position);
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setMessage("Confirmar conclusão?");
+
+                    builder.setPositiveButton("Sim", (dialog, which) -> {
+                        long result = myDB.deleteTask(task);
+                        if (result != 0)
+                            adapter.deleteTask(position);
+                        else
+                            Toast.makeText(context, "Falha ao deletar a tarefa.", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    });
+                    builder.setNegativeButton("Não", (dialog, which) -> {
+                        dialog.dismiss();
+                    });
+
+                    builder.show();
+                };
+            }
         });
     }
 
