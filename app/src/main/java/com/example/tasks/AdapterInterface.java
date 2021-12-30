e1e3bd982616cc99e16204b3e0fd4ab75c8a57fa
omouravictor <omouravictor@gmail.com>
2021-12-29 22:30:19 -0300

    getBtnCompleteOnClickListener() created on AdapterInterface

--- app/src/main/java/com/example/tasks/AdapterInterface.java
+++ app/src/main/java/com/example/tasks/AdapterInterface.java
@@ -2,9 +2,11 @@ package com.example.tasks;
 
 import android.view.View;
 
-/* This interface allows you to add the onClickListener view item from where it is called, using "position"
+/* This interface allows you to add onClickListeners view item from where it is called, using "position"
 parameter from the onBindViewHolder function in TaskAdapter, instead of add directly inside onBindViewHolder.
-It was created that way to make better use of registerForActivityResult in MainActivity */
+It was created that way to make better use of registerForActivityResult in MainActivity for Adapter ItemView OnClickListener*/
 public interface AdapterInterface {
     View.OnClickListener getOnClickListener(int position);
+
+    View.OnClickListener getBtnCompleteOnClickListener(int position);
 }
