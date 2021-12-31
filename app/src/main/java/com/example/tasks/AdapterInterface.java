4da012b41b35a8fe4c7700ac2deafb5fe9d89e11
omouravictor <omouravictor@gmail.com>
2021-12-31 00:20:00 -0300

    Comment updated

--- app/src/main/java/com/example/tasks/AdapterInterface.java
+++ app/src/main/java/com/example/tasks/AdapterInterface.java
@@ -2,9 +2,9 @@ package com.example.tasks;
 
 import android.view.View;
 
-/* This interface allows you to add onClickListeners view item from where it is called, using "position"
-parameter from the onBindViewHolder function in TaskAdapter, instead of add directly inside onBindViewHolder.
-It was created that way to make better use of registerForActivityResult in MainActivity for Adapter ItemView OnClickListener*/
+/* This interface allows you to add the TaskAdapter's onClickListeners from where it's called using the "position" parameter
+from the onBindViewHolder function in TaskAdapter, instead of adding directly inside the onBindViewHolder.
+It was created that way to make better use of ActivityResultLauncher in MainActivity */
 public interface AdapterInterface {
     View.OnClickListener getOnClickListener(int position);
 
