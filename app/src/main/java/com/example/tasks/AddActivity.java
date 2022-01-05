30cfafe3ef107f98090f073d54b92ca71c602153
omouravictor <omouravictor@gmail.com>
2022-01-05 08:33:38 -0300

    Optimization done

--- app/src/main/java/com/example/tasks/AddActivity.java
+++ app/src/main/java/com/example/tasks/AddActivity.java
@@ -1,12 +1,12 @@
 package com.example.tasks;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AddActivity extends AppCompatActivity {
 
