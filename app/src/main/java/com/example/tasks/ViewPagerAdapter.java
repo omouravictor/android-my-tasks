d4a45a1d4436c6b36ee25d88423e47d8ba233ad1
omouravictor <omouravictor@gmail.com>
2022-02-07 22:50:56 -0300

    ViewPagerAdapter created

---
+++ app/src/main/java/com/example/tasks/ViewPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.tasks;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    private final ArrayList<Fragment> fragments = new ArrayList<>();
+    private final ArrayList<String> titles = new ArrayList<>();
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+
+    public ArrayList<String> getTitles() {
+        return titles;
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragments.add(fragment);
+        titles.add(title);
+    }
+}
