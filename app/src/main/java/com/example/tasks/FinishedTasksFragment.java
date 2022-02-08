d6c489e30411ae0524e353c0075b7599a145ded6
omouravictor <omouravictor@gmail.com>
2022-02-08 17:12:07 -0300

    finishedTasksAdapter variable created + constructor updated + getFinishedTasksAdapter created

--- app/src/main/java/com/example/tasks/FinishedTasksFragment.java
+++ app/src/main/java/com/example/tasks/FinishedTasksFragment.java
@@ -1,25 +1,22 @@
 package com.example.tasks;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.ArrayList;
-
 public class FinishedTasksFragment extends Fragment {
 
     View view;
     RecyclerView recyclerView;
-    TaskAdapter adapter;
+    TaskAdapter finishedTasksAdapter;
 
-    public FinishedTasksFragment(ArrayList<TaskModel> finishedTasks) {
-        adapter = new TaskAdapter(finishedTasks);
+    public FinishedTasksFragment(TaskAdapter tasksOnHoldAdapter) {
+        this.finishedTasksAdapter = new TaskAdapter(tasksOnHoldAdapter);
     }
 
     @Override
@@ -28,8 +25,11 @@ public class FinishedTasksFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_finished_tasks, container, false);
         recyclerView = view.findViewById(R.id.rvFinishedTasks);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(finishedTasksAdapter);
         return view;
     }
 
+    public TaskAdapter getFinishedTasksAdapter() {
+        return finishedTasksAdapter;
+    }
 }
