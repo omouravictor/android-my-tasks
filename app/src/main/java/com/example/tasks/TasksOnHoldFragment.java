b838c40556e5dbcca5e6bfb1981cc74657d28eff
omouravictor <omouravictor@gmail.com>
2022-02-07 22:42:36 -0300

    TasksOnHoldFragment created

---
+++ app/src/main/java/com/example/tasks/TasksOnHoldFragment.java
@@ -0,0 +1,34 @@
+package com.example.tasks;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class TasksOnHoldFragment extends Fragment {
+
+    View view;
+    RecyclerView recyclerView;
+    TaskAdapter adapter;
+
+    public TasksOnHoldFragment(TaskAdapter adapter) {
+        this.adapter = adapter;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_tasks_on_hold, container, false);
+        recyclerView = view.findViewById(R.id.rvTasksOnHold);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+
+}
