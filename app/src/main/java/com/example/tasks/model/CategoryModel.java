9faee20fad4308e4626b483a5ef7ba453ae03ed0
omouravictor <omouravictor@gmail.com>
2022-04-09 19:47:52 -0300

    refactor: unnecessary tasksList attribute removed

--- app/src/main/java/com/example/tasks/model/CategoryModel.java
+++ app/src/main/java/com/example/tasks/model/CategoryModel.java
@@ -5,25 +5,16 @@ import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
 
-import java.util.ArrayList;
-
 public class CategoryModel implements Parcelable {
 
     private String name;
-    private ArrayList<TaskModel> tasksList;
 
     public CategoryModel(String name) {
         this.name = name;
     }
 
-    public CategoryModel(String name, ArrayList<TaskModel> tasksList) {
-        this.name = name;
-        this.tasksList = tasksList;
-    }
-
     protected CategoryModel(@NonNull Parcel in) {
         name = in.readString();
-        tasksList = in.readArrayList(TaskModel.class.getClassLoader());
     }
 
     public static final Creator<CategoryModel> CREATOR = new Creator<CategoryModel>() {
@@ -46,7 +37,6 @@ public class CategoryModel implements Parcelable {
     @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
         dest.writeString(name);
-        dest.writeList(tasksList);
     }
 
     public String getName() {
@@ -57,11 +47,4 @@ public class CategoryModel implements Parcelable {
         this.name = name;
     }
 
-    public ArrayList<TaskModel> getTasksList() {
-        return tasksList;
-    }
-
-    public void setTasksList(ArrayList<TaskModel> tasksList) {
-        this.tasksList = tasksList;
-    }
 }
