ace93e4ab1a2de1f7a2e4fb9582379593a1d7aa1
omouravictor <omouravictor@gmail.com>
2022-04-09 19:45:00 -0300

    feat: categoryName attribute added on TaskModel

--- app/src/main/java/com/example/tasks/model/TaskModel.java
+++ app/src/main/java/com/example/tasks/model/TaskModel.java
@@ -9,32 +9,30 @@ public class TaskModel implements Parcelable {
 
     private long id;
     private String name;
-    private String finishedDate;
     private String expirationDate;
+    private String finishedDate;
+    private String categoryName;
 
-    public TaskModel(String name, String expirationDate) {
+    public TaskModel(String name, String expirationDate, String categoryName) {
         this.name = name;
         this.expirationDate = expirationDate;
+        this.categoryName = categoryName;
     }
 
-    public TaskModel(String name, String expirationDate, String finishedDate) {
-        this.name = name;
-        this.expirationDate = expirationDate;
-        this.finishedDate = finishedDate;
-    }
-
-    public TaskModel(long id, String name, String expirationDate, String finishedDate) {
+    public TaskModel(long id, String name, String expirationDate, String finishedDate, String categoryName) {
         this.id = id;
         this.name = name;
-        this.finishedDate = finishedDate;
         this.expirationDate = expirationDate;
+        this.finishedDate = finishedDate;
+        this.categoryName = categoryName;
     }
 
     protected TaskModel(@NonNull Parcel in) {
         id = in.readLong();
         name = in.readString();
-        finishedDate = in.readString();
         expirationDate = in.readString();
+        finishedDate = in.readString();
+        categoryName = in.readString();
     }
 
     public static final Creator<TaskModel> CREATOR = new Creator<TaskModel>() {
@@ -58,8 +56,9 @@ public class TaskModel implements Parcelable {
     public void writeToParcel(@NonNull Parcel dest, int flags) {
         dest.writeLong(id);
         dest.writeString(name);
-        dest.writeString(finishedDate);
         dest.writeString(expirationDate);
+        dest.writeString(finishedDate);
+        dest.writeString(categoryName);
     }
 
     public void finish(String finishedDate) {
@@ -104,5 +103,13 @@ public class TaskModel implements Parcelable {
 
     public void setFinishedDate(String finishedDate) {
         this.finishedDate = finishedDate;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
     }
 }
